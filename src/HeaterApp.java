c3df5b1b32cb168244478e4b8f872a302e39c376
Admin <marcin123@gmail.com>
2023-02-04 19:22:43 +0100

    a

--- src/HeaterApp.java
+++ src/HeaterApp.java
@@ -1,5 +1,7 @@
 public class HeaterApp {
     public static void main(String[] args) {
-
+        Heater heat1= new Heater();
+        upOrDown knob = new upOrDown();
+        knob.adjustTemp(heat1);
     }
 }
--- src/upOrDown.java
+++ src/upOrDown.java
@@ -1,12 +1,16 @@
 class upOrDown {
+
+
     public void adjustTemp(Heater heater){
         if (heater.getTempActu()< heater.getTempTarg()){
             while (heater.getTempActu()!= heater.getTempTarg()){
                 heater.setTempActu(heater.getTempActu()+1);
+                System.out.println(heater.getTempActu());
             }
         }else {
             while (heater.getTempActu()!= heater.getTempTarg()){
                 heater.setTempActu(heater.getTempActu()-1);
+                System.out.println(heater.getTempActu());
             }
         }
     }
